cb8ba30548bc26af48641e6622aee7bd61fc760e
Romain Pontacq <romain.pontacqpro@gmail.com>
2022-10-24 21:08:31 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <todo-list/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// Here for importing components
+import TodoList from "./components/TodoList.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TodoList : TodoList
   }
 }
 </script>
 
 <style>
+body{
+  margin: 0px;
+  padding: 0px;
+  box-sizing: border-box;
+
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  border: 2px solid green;
+  
 }
 </style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,39 @@
+<template>
+    <!-- Partie HTML -->
+    <p>TodoList here</p>
+    <todo-text-field titleBtn="ajouter tâche" @adding="addTask"/>
+    <ul>
+        <li v-for="task in tasks" :key="task">
+            {{task}}
+        </li>
+    </ul>
+    
+</template>
+
+<script>
+    //  Partie JS - Vue
+    import TodoTextField from "./TodoTextField.vue"
+    export default {
+        name: 'TodoList',
+        data() {
+            return{
+                tasks : []
+            }
+        },
+        components : {
+            TodoTextField : TodoTextField,
+        },
+        methods:{
+            addTask : function(value){
+                this.tasks.push(value)
+                console.log("tableau =" + this.tasks)
+                
+            }
+        }
+            
+    }
+</script>
+
+<style>
+    /* Partie CSS */
+</style>
